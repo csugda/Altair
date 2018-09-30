03390a9d0be20182681f9ddc80698e800785e690
JosiahMay <Josiah@pleasefixit.com>
2018-09-30 14:55:15 -0600

    Added basic unit movement and end of path logic

---
+++ Altair/Assets/Scripts/Units/UnitController.cs
@@ -0,0 +1,132 @@
+﻿using Assets.Scripts.Map;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject nextNodeInPath;
+
+    [SerializeField]
+    private GameObject startLocation;
+    [SerializeField]
+    private float speed = .10f;
+    [SerializeField]
+    private PathType endGoal;
+
+
+    private void Start()
+    {
+        this.transform.position = startLocation.transform.position;
+    }
+
+
+
+    void FixedUpdate()
+    {
+        
+        if (nextNodeInPath == null)
+        {
+            GetNextPathNode();
+            if (nextNodeInPath == null && this.transform.position != startLocation.transform.position)
+            {
+                // We've run out of path!
+                Destroy(this.gameObject);
+                return;
+            }
+        }
+
+        
+        /*if (this.gameObject.transform.position == nextNodeInPath.transform.position)
+        {
+            //PathNode next = nextNodeInPath.GetComponent(typeof(PathNode)) as PathNode;
+
+            PathNode next = nextNodeInPath.GetComponent<PathNode>().GetNextNode(endGoal);
+            if(next != null)
+            {
+                nextNodeInPath = next.gameObject;
+            }
+            
+            //Debug.Log("Moving to next node\nLooking for (" + next.X + " " + next.Y + ")");
+            
+            
+        }*/
+        Vector3 dir = nextNodeInPath.transform.position - this.transform.localPosition;
+        float distThisFrame = speed * Time.deltaTime;
+        if (dir.magnitude <= distThisFrame)
+        {
+            // We reached the node
+            GetNextPathNode();
+        }
+
+        if (nextNodeInPath != null)
+        {
+            //  transform.position = Vector3.Slerp(this.gameObject.transform.position, nextNodeInPath.transform.position, speed);
+
+
+
+            Quaternion targetRotation;
+
+            transform.Translate(dir.normalized * distThisFrame, Space.World);
+            if (!dir.Equals(Vector3.zero))
+            {
+                targetRotation = Quaternion.LookRotation(dir);
+            } else
+            {
+                targetRotation = Quaternion.identity;
+            }
+            
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRotation, Time.deltaTime * 5);
+        }
+        /*
+        Vector3 dir = nextNodeInPath.transform.position - this.transform.localPosition;
+
+        float distThisFrame = speed * Time.deltaTime;
+
+        if (dir.magnitude <= distThisFrame)
+        {
+            // We reached the node
+            nextNodeInPath = null;
+        }
+        else
+        {
+            // TODO: Consider ways to smooth this motion.
+
+            // Move towards node
+            transform.Translate(dir.normalized * distThisFrame, Space.World);
+            Quaternion targetRotation = Quaternion.LookRotation(dir);
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRotation, Time.deltaTime * 5);
+        }*/
+
+
+
+
+    }
+
+    private void GetNextPathNode()
+    {
+        try
+        {
+
+
+
+            PathNode next = nextNodeInPath.GetComponent<PathNode>().GetNextNode(endGoal);
+            if (next == null && this.transform.position != startLocation.transform.position)
+            {
+                nextNodeInPath = null;
+                
+            } 
+            if(next != null)
+            {
+                nextNodeInPath = next.gameObject;
+            }
+            
+        } catch(NullReferenceException){
+            Debug.Log("Could not set next node.");
+            return;
+        }
+        
+    }
+}
---
+++ Altair/Assets/Scripts/WorldGen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGen : MonoBehaviour {
+
+    private int NumColumns = 15;
+    private int NumRows = 15;
+
+    public GameObject tilePrefab;
+    //public GameObject towerPrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        for (int column = 0; column < NumColumns; column++)
+        {
+            for (int row = 0; row < NumRows; row++)
+            {
+                
+                GameObject newTile = GameObject.Instantiate(tilePrefab, new Vector3(column, 0, row), Quaternion.Euler(0,0,0));
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
