6c9fdcacdc4434c33c8da777f226766f4315b44f
smilewood <milewood@gmail.com>
2018-10-03 09:39:17 -0600

    starting tower placment logic. this involved a bunch of rewriting the pathfinding stuff to make it work with new requirements. Also rearranged many scripts into diferent folders.

---
+++ Altair/Assets/Scripts/Map/Construction/TowerBuildSite.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Scripts.Map.Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Map.Construction
+{
+
+    public class TowerBuildSite : MonoBehaviour
+    {
+        PrefabSet towers;
+        
+        public void CheckBuildTower()
+        {
+            this.gameObject.transform.parent.GetComponentInChildren<PathNode>().impassable = true;
+            EnemyPathRegen.UpdateEnemyPaths.Invoke(false);
+            AStar.FindPath();
+            this.gameObject.transform.parent.GetComponentInChildren<PathNode>().impassable = false;
+        }
+
+        public void BuildTower()
+        {
+            this.gameObject.transform.parent.GetComponentInChildren<PathNode>().impassable = true;
+            EnemyPathRegen.UpdateEnemyPaths.Invoke(false);
+            AStar.FindPath();
+            Instantiate(towers.RandomPrefab());
+        }
+    }
+}
---
+++ Altair/Assets/Scripts/Map/Pathfinding/PathingTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Map.Pathfinding
+{
+    public class PathingTest : MonoBehaviour
+    {
+        public PathNode start, g1, g2, g3;
+        // Use this for initialization
+        public void FindPath()
+        {
+            
+        }
+        private void Start()
+        {
+            AStar.SetStart(start);
+            AStar.SetGoal(PathType.A, g1);
+            AStar.SetGoal(PathType.B, g2);
+            AStar.SetGoal(PathType.C, g3);
+            AStar.GeneratePath();
+        }
+
+    }
+}
