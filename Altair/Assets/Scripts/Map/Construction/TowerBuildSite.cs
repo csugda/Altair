4fb312baa96040b1ecdefea2ce648d8416c77f18
smilewood <milewood@gmail.com>
2018-10-03 17:12:59 -0600

    Tower placement part two! Towers preview where they will be built, and tint red when that spot would cut off a path. TODO: there may be an issue with the pathfinding script being called many, many times when it should only be called three times. TODO: make sure that paths are being upddated for any enemy currently on the map, not just static spawn->goal paths.

--- Altair/Assets/Scripts/Map/Construction/TowerBuildSite.cs
+++ Altair/Assets/Scripts/Map/Construction/TowerBuildSite.cs
@@ -9,21 +9,55 @@ namespace Assets.Scripts.Map.Construction
     public class TowerBuildSite : MonoBehaviour
     {
         PrefabSet towers;
-        
-        public void CheckBuildTower()
+        bool towerExists = false;
+        public GameObject previewPrefab;
+        private GameObject preview;
+        private bool buildable;
+        private void Start()
         {
-            this.gameObject.transform.parent.GetComponentInChildren<PathNode>().impassable = true;
-            EnemyPathRegen.UpdateEnemyPaths.Invoke(false);
-            AStar.FindPath();
-            this.gameObject.transform.parent.GetComponentInChildren<PathNode>().impassable = false;
+            AStar.PathNotFound.AddListener(this.PreviewError);
+            towers = GameObject.Find("TowerSet").GetComponent<PrefabSet>();
         }
 
-        public void BuildTower()
+        private void PreviewError()
         {
+
+            if (preview == null) return;
+            this.buildable = false;
+            preview.GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        public void OnMouseEnter()
+        {
+            //Debug.Log("MouseEnter: " + this.gameObject.transform.parent.name);
+            if (towerExists || preview != null) return;
             this.gameObject.transform.parent.GetComponentInChildren<PathNode>().impassable = true;
+            this.buildable = true;
+            preview = Instantiate(previewPrefab, this.gameObject.transform.parent);
             EnemyPathRegen.UpdateEnemyPaths.Invoke(false);
             AStar.FindPath();
-            Instantiate(towers.RandomPrefab());
+        }
+        
+        private void OnMouseExit()
+        {
+            //Debug.Log("MouseExit: " + this.gameObject.transform.parent.name);
+            if (!towerExists)
+                this.gameObject.transform.parent.GetComponentInChildren<PathNode>().impassable = false;
+            if (preview == null) return;
+            Destroy(preview);
+            preview = null;
+        }
+
+        public void OnMouseDown()
+        {
+            if (towerExists || !this.buildable) return;
+            this.gameObject.transform.parent.GetComponentInChildren<PathNode>().impassable = true;
+            EnemyPathRegen.UpdateEnemyPaths.Invoke(true);
+            AStar.GeneratePath();
+            Instantiate(towers.RandomPrefab(), this.gameObject.transform.parent);
+            this.towerExists = true;
+            this.OnMouseExit();
+
         }
     }
 }
