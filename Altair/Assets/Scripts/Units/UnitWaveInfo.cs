bbfdedce5ef6a378f839059319d647364abdd0f8
JosiahMay <Josiah@pleasefixit.com>
2018-10-25 18:56:14 -0600

    Added a unit spawner, wave controller, and way to read wave files

---
+++ Altair/Assets/Scripts/Units/UnitWaveInfo.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Collections.Generic;
+
+/// <summary>
+/// The units info for the wave, the unit to spawn, the amount to spawn, the time between spawns, when to next spawn
+/// </summary>
+public class UnitWaveInfo  {
+
+    public int unitType;
+    public int spawnAmount;
+    public double timeBetweenSpawns;
+    public double nextSpawn;
+
+    /// <summary>
+    /// Constructor for setting all the states
+    /// </summary>
+    /// <param name="unitType"></param> what unit to spawn
+    /// <param name="spawnAmount"></param> how many units to spawn
+    /// <param name="timeBetweenSpawns"></param> time between spawns
+    /// <param name="nextSpawn"></param> when to next spawn a unit
+    public UnitWaveInfo(int unitType, int spawnAmount, double timeBetweenSpawns, double nextSpawn)
+    {
+        this.unitType = unitType;
+        this.spawnAmount = spawnAmount;
+        this.timeBetweenSpawns = timeBetweenSpawns;
+        this.nextSpawn = nextSpawn;
+    }
+
+    override
+    public string ToString()
+    {
+        return "UnitType: " + unitType + " SpawnAmount: " + spawnAmount + " TimeBetweenSpawns: " + timeBetweenSpawns + " NextSpawn: " + nextSpawn;
+    }
+	
+}
+
