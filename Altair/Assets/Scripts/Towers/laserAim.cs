a2772baf1935fde6155a39c9871e72beb1610a23
Jonathan Griego <31517248+JonathanGriego@users.noreply.github.com>
2018-11-08 10:17:17 -0700

    updated tower with a cooldown to disable laser while changing targets

--- Altair/Assets/Scripts/Towers/laserAim.cs
+++ Altair/Assets/Scripts/Towers/laserAim.cs
@@ -2,28 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class laserAim : MonoBehaviour {
-    public GameObject laser;
-    public LineRenderer laserBeam;
-    public TargetAcquisition targeter;
-    public float height = 1f;
+public class laserAim : MonoBehaviour
+{
+	public GameObject laser;
+	public LineRenderer laserBeam;
+	public TargetAcquisition targeter;
+	public float height = 1f;
+
+	private float coolDown;
+	public float coolDownVal = .175f;
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
+		coolDown = coolDownVal;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-        if (targeter.target == null)
-        {
-            laser.SetActive(false);
-        }
-        else
-        {
-            laser.SetActive(true);
-            Transform target = targeter.target;
-            LineRenderer laserBeam = laser.GetComponentInChildren<LineRenderer>();
-            laserBeam.SetPosition(0, laserBeam.gameObject.transform.position);
-            laserBeam.SetPosition(1, new Vector3(target.position.x, height, target.position.z));
-        }
+	void Update()
+	{
+		if (targeter.target == null)
+		{
+			laser.SetActive(false);
+			coolDown = coolDownVal;
+		}
+		else
+		{
+			if (coolDown > 0)
+			{
+				coolDown -= Time.deltaTime;
+			}
+			else
+			{
+				laser.SetActive(true);
+				Transform target = targeter.target;
+				LineRenderer laserBeam = laser.GetComponentInChildren<LineRenderer>();
+				laserBeam.SetPosition(0, laserBeam.gameObject.transform.position);
+				laserBeam.SetPosition(1, new Vector3(target.position.x, height, target.position.z));
+			}
+
+		}
 	}
 }
