8320c2369b2b5dfa902ca5e0d80cc14f6fdc20a1
smilewood <milewood@gmail.com>
2018-11-07 21:27:18 -0700

    Import new UI assets and implement UI scene. Implement build teams v1

---
+++ Altair/Assets/DisableOnStart.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class DisableOnStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.gameObject.SetActive(false);
+	}
+	
+}
---
+++ Altair/Assets/Scripts/Map/Construction/BuildTeam.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Map.Construction
+{
+
+    public class BuildTeam : MonoBehaviour
+    {
+
+        public bool busy = false, buildMode = false;
+
+        public void StartBuildMode()
+        {
+            if (buildMode || busy) return;
+            TowerBuildSite.StartBuildMode(this);
+            this.buildMode = true;
+        }
+
+        public void EndBuildMode()
+        {
+            TowerBuildSite.EndBuildMode();
+            this.buildMode = false;
+        }
+
+        public void BeginConstruction(TowerBuildSite site)
+        {
+            this.busy = true;
+            this.EndBuildMode();
+        }
+
+        public void FinishConstruction()
+        {
+            this.busy = false;
+        }
+    }
+}
