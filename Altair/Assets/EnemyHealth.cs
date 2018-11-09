618bf936db765f36cd6313e76422c5630ea1354b
mvoyles <mvoyles6@gmail.com>
2018-11-08 17:56:43 -0700

    HealthforEnemies
    Made public variable for health. Made callable method that allows health to be decreased by certain amount

---
+++ Altair/Assets/EnemyHealth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+
+    private Rigidbody rb;
+    public int health = 3;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Time.deltaTime, 0, 0);
+    }
+
+    void decreaseHealth(int healthAmount)
+    {
+        health = health - healthAmount;
+    }
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Damage"))
+    //    {
+    //        Destroy(other.gameObject);
+    //        health = health - 1;
+    //    }
+    //    if(health == 0)
+    //    {
+    //        Destroy(this.gameObject);
+    //    }
+    //}
+    
+}
+
