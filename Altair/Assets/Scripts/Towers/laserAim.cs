286647c2dc415227f8c9342d06ed784ae72fc4fc
Jonathan Griego <31517248+JonathanGriego@users.noreply.github.com>
2018-11-08 19:10:19 -0700

    changed from hard cooldown to Quaternion based power down for Laser

--- Altair/Assets/Scripts/Towers/laserAim.cs
+++ Altair/Assets/Scripts/Towers/laserAim.cs
@@ -9,37 +9,27 @@ public class laserAim : MonoBehaviour
 	public TargetAcquisition targeter;
 	public float height = 1f;
 
-	private float coolDown;
-	public float coolDownVal = .175f;
 	// Use this for initialization
 	void Start()
 	{
-		coolDown = coolDownVal;
+		laser.SetActive(false);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		if (targeter.target == null)
+		if (targeter.target == null || Mathf.Abs(((targeter.rotatingPart.transform.rotation.eulerAngles.y + 360)
+			- (targeter.lookRot.eulerAngles.y + 360))) > 5)
 		{
 			laser.SetActive(false);
-			coolDown = coolDownVal;
 		}
 		else
 		{
-			if (coolDown > 0)
-			{
-				coolDown -= Time.deltaTime;
-			}
-			else
-			{
 				laser.SetActive(true);
 				Transform target = targeter.target;
 				LineRenderer laserBeam = laser.GetComponentInChildren<LineRenderer>();
 				laserBeam.SetPosition(0, laserBeam.gameObject.transform.position);
 				laserBeam.SetPosition(1, new Vector3(target.position.x, height, target.position.z));
-			}
-
 		}
 	}
 }
