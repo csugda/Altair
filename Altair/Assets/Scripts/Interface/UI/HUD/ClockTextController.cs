a1b92168d40b89aa600105116ca09ded7c48e38b
smilewood <milewood@gmail.com>
2018-11-14 21:12:33 -0700

    Working clock, working next wave button

---
+++ Altair/Assets/Scripts/Interface/UI/HUD/ClockTextController.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts.Units;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ClockTextController : MonoBehaviour {
+    public double clockTime = 00;
+	// Use this for initialization
+	void Start () {
+        UnitSpawnController.newWaveStart.AddListener(this.updateClockTime);
+	}
+
+    private void Update()
+    {
+        clockTime -= Time.deltaTime;
+        int sec = (int)clockTime;
+        int ms = (int)((clockTime - sec) * 10);
+        this.gameObject.GetComponent<Text>().text = sec + ":" + ms;
+    }
+
+    public void updateClockTime(double time)
+    {
+        this.clockTime = time;
+    }
+	
+	
+}
