0ed593c09a96ddd1979b80e7f6a9663a1e2a3606
smilewood <milewood@gmail.com>
2018-11-14 21:22:25 -0700

    Add enemyHealth and temp laser damage script

--- Altair/Assets/Scripts/Towers/laserAim.cs
+++ Altair/Assets/Scripts/Towers/laserAim.cs
@@ -2,37 +2,41 @@
 
 public class laserAim : MonoBehaviour
 {
-	public GameObject laser;
-	public LineRenderer laserBeam;
-	public TargetAcquisition targeter;
-	public float height = 1f;
+    public GameObject laser;
+    public LineRenderer laserBeam;
+    public TargetAcquisition targeter;
+    public float height = 1f;
 
-	// Use this for initialization
-	void Start()
-	{
-		laser.SetActive(false);
-	}
+    public float dps = 50;
 
-	// Update is called once per frame
-	void Update()
-	{
+    // Use this for initialization
+    void Start()
+    {
+        laser.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (!targeter.isActiveAndEnabled)
         {
             laser.SetActive(false);
             return;
         }
-		if (targeter.target == null || Mathf.Abs(((targeter.rotatingPart.transform.rotation.eulerAngles.y + 360)
-			- (targeter.lookRot.eulerAngles.y + 360))) > 5)
-		{
+        if (targeter.target == null || Mathf.Abs(((targeter.rotatingPart.transform.rotation.eulerAngles.y + 360)
+            - (targeter.lookRot.eulerAngles.y + 360))) > 5)
+        {
             laser.SetActive(false);
         }
-		else
-		{
-				laser.SetActive(true);
-				Transform target = targeter.target;
-				LineRenderer laserBeam = laser.GetComponentInChildren<LineRenderer>();
-				laserBeam.SetPosition(0, laserBeam.gameObject.transform.position);
-				laserBeam.SetPosition(1, new Vector3(target.position.x, height, target.position.z));
-		}
-	}
+        else
+        {
+            laser.SetActive(true);
+            Transform target = targeter.target;
+            LineRenderer laserBeam = laser.GetComponentInChildren<LineRenderer>();
+            laserBeam.SetPosition(0, laserBeam.gameObject.transform.position);
+            laserBeam.SetPosition(1, new Vector3(target.position.x, height, target.position.z));
+            target.gameObject.GetComponent<EnemyHealth>().takeDammage.Invoke(dps * Time.deltaTime);
+
+        }
+    }
 }
